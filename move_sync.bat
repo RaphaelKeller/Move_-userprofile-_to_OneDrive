f40c9017c8fe002e8067f358a9a42193fefa2fab
Raphael Keller <95104548+RaphaelKeller@users.noreply.github.com>
2022-03-13 18:45:49 +0100

    Update

---
+++ move_sync.bat
@@ -0,0 +1,98 @@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GPYYJYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#G5YYJJJYYYYYYYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#G5YJJJJYYYYYJJYYYYYYYYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BP5YJJJJYYYYJJYY5GG5YYYYYYYYYYYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@#BP5YJJJJJJJJJJYY5G#&@@@@&#GPYYYYYYYYYYYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@#BP5YJJJJJJJJJJYYPG#&@@@@@@@@@@@@&#GP5YYYYYYYYYYY5PGB&@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@#BPYJJJJJJJJJJJYYPG#&@@@@@@@@@@@@@@@@@@@@&#BP5YYYYYYYYYYY5PGB&@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@&#GPYJJJJJJJJJJJYYPB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#BP55YYY55555555PGB#@@@@@@@@@@@@@@@
+::@@@@@@@@@@&#G5YJJJJJJJJJJJY5PB#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#BGP5YY555555555PGB#&@@@@@@@@@@
+::@@@@@@&#G5YJJJJJJJJJJJY5PB#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GP555555555555PGB#&@@@@@@
+::@@@@P5JJJJJJJJJJJJY5PB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GP555555555555PGB@@@@
+::@@@@JJJJJJJJJJY5PB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BP55555555555@@@@
+::@@@@JJJJJJJYG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BP5555555@@@@
+::@@@@JJJJJJJP@@@@@@@@@@@@@@@@@@@@@@@&5555555555555555555555555PPPPPPPPPPG@@@@@@@@@@@@@@@@B5555555@@@@
+::@@@@JJJJJJJP@@@@@@@@@@@@@@@@@@@@@@@#YYYYYYYYYYYYYYYYYYYYYYYYYYYY5555555G@@@@@@@@@@@@@@@@B5555555@@@@
+::@@@@JJJJJJJP@@@@@@@@@@@@@@@@@@@@@@@&YYYYYYYYYYYYYYYYYYYYYYYYYYYY5555555G@@@@@@@@@@@@@@@@B555555P@@@@
+::@@@@JJJJJJJG@@@@@@@@@@@@@@@@#BBBBBBBPPPPPPPPPPPPPPPPPPPPPGGGGGGGGGGGGGGB@@@@@@@@@@@@@@@@B55555PP@@@@
+::@@@@JJJJJJJG@@@@@@@@@@@@@@@@PYYYYYY5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B555PPPP@@@@
+::@@@@JJJJJJJG@@@@@@@@@@@@@@@@PYYYYYY5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@B5PPPPPP@@@@
+::@@@@JJJJJJJG@@@@@@@@@@@@@@@@PYYYYYY5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@JJJJJJJG@@@@@@@@@@@@@@@@PYYYYYY5###################################&@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@JJJJJJYG@@@@@@@@@@@@@@@@PYYYYYYYYYYYYYYYYYYYYYY55555555555555555555G@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@JJJJYYYG@@@@@@@@@@@@@@@@PYYYYYYYYYYYYYYYYYYY55555555555555555555555G@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@JJYYYYYG@@@@@@@@@@@@@@@@PYYYYYYYYYYYYYYYYY5555555555555555555555555G@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@JYYYYYYG@@@@@@@@@@@@@@@@PYYYYYY5###################################&@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYYG@@@@@@@@@@@@@@@@PYYYYYY5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYYG@@@@@@@@@@@@@@@@PYYYYYY5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYYG@@@@@@@@@@@@@@@@PYYYYYY5@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYYG@@@@@@@@@@@@@@@@#BBBBBBBPGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGB@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYYG@@@@@@@@@@@@@@@@@@@@@@@&YYYYYY55555555555555555555555555555G@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYYG@@@@@@@@@@@@@@@@@@@@@@@&Y5555555555555555555555555555555555G@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYYG@@@@@@@@@@@@@@@@@@@@@@@&PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPB@@@@@@@@@@@@@@@@BPPPPPPP@@@@
+::@@@@YYYYYYY5G#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGPPPPPPP@@@@
+::@@@@YYYYYYYYYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGPPPPPPPPPPP@@@@
+::@@@@GPYYYYYYYYYYYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGPPPPPPPPPPPPGGB@@@@
+::@@@@@@&#GP5YYYYYYYYYYY5PG#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGPPPPPPPPPPPPGGB&&@@@@@@
+::@@@@@@@@@@&#BP5YYYYYYYYYYY5PGB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGPPPPPPPPPPPPGB#&@@@@@@@@@@@
+::@@@@@@@@@@@@@@&#BP55YYYY5555YY5PGB#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GPPPPPPPPPPPPPGB#&@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@#BGP5YY555555555PGB#&@@@@@@@@@@@@@@@@@@@@@&BGPPPPPPPPPPPPPGB#&@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@&BGP555555555555PGB#&@@@@@@@@@@@@&#BGPPPPPPPPPPPPPGB#&@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GP555555555555PGB#&@@@@&#BGPP5PPPPPPPPPPGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#GP555555555555PGBBGP555PPPPPPPPPGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGP555555555555PPPPPPPPPGB#&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&#BGP55555PPPP5PPPGB&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&BGPP55PPGB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&BB&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+::written by Raphael Keller
+
+
+mkdir "C:\source"
+mkdir "%Onedrive%\PCSync"
+mkdir "%USERPROFILE%\OneDrive"
+set Onedrive = "%USERPROFILE%\OneDrive"
+
+robocopy "C:\source\Scripts" "%onedrive%\Scripts" /E /MT:16 /move
+mklink /J "C:\source\Scripts" "%onedrive%\Scripts"
+
+robocopy "%userprofile%\Desktop" "%onedrive%\PCSync\Desktop" /E /MT:16 /move
+mklink /J "%userprofile%\Desktop" "%onedrive%\PCSync\Desktop"
+
+robocopy "%userprofile%\Music" "%onedrive%\PCSync\Music" /E /MT:16 /move
+
+robocopy "%userprofile%\repos" "%onedrive%\Visual Studio\repos" /MT:16 /E /move
+mklink /J "%userprofile%\repos" "%onedrive%\Visual Studio\repos"
+
+robocopy "%userprofile%\Saved Games" "%onedrive%\PCSync\Saved Games" /MT:16 /E /move
+
+robocopy "%userprofile%\source" "%onedrive%\Visual Studio\source" /E /MT:16 /move
+mklink /J "%userprofile%\source" "%onedrive%\Visual Studio\source"
+
+robocopy "%userprofile%\Videos" "%onedrive%\PCSync\Videos" /E /MT:16 /move
+
+robocopy "%onedrive%\Documents" "%onedrive%\PCSync\Documents" /E /MT:16 /move
+robocopy "%userprofile%\Documents" "%onedrive%\PCSync\Documents" /E /MT:16 /move
+
+robocopy "%onedrive%\Pictures" "%onedrive%\PCSync\Pictures" /E /MT:16 /move
+robocopy "%userprofile%\Pictures" "%onedrive%\PCSync\Pictures" /E /MT:16 /move
+
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "My Music" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Music"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /f /v "My Music" /t REG_SZ /d "%Onedrive%\PCSync\Music"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "{A0C69A99-21C8-4671-8703-7934162FCF1D}" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Music"
+
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /f /v "{4C5C32FF-BB9D-43B0-B5B4-2D72E54EAAA4}" /t REG_SZ /d "%Onedrive%\PCSync\Saved Games"
+
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "My Video" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Videos"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /f /v "My Video" /t REG_SZ /d "%Onedrive%\PCSync\Videos"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "{35286A68-3C57-41A1-BBB1-0EAE73D76C95}" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Videos"
+
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "Personal" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Documents"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /f /v "Personal" /t REG_SZ /d "%Onedrive%\PCSync\Documents"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "{F42EE2D3-909F-4907-8871-4C22FC0BF756}" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Documents"
+
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "My Pictures" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Pictures"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /f /v "My Pictures" /t REG_SZ /d "%Onedrive%\PCSync\Pictures"
+reg.exe ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /f /v "{0DDD015D-B06C-45D5-8C4C-F59713854639}" /t REG_EXPAND_SZ /d "%Onedrive%\PCSync\Pictures"
+
+pause
